bcd303c04a3657fb7f6a247343c335de28172747
Fazalwahab12 <wahabdir567@gmail.com>
2024-06-25 05:03:01 -0700

    clean

--- src/components/corousel.js
+++ src/components/corousel.js
@@ -64,20 +64,18 @@ const Carousel = () => {
                 </div>
                 <div className="post-info text-center">
                     <div className="bg-white m-auto position-relative post-header z-1 pb-0">
-                        {/* Start Meta Info */}
+                     
                         <div className="align-items-start flex-wrap fs-15 gap-2 hstack text-capitalize mb-1 justify-content-center">
-                            <div className="text-primary fw-medium">Unparalleled Amenities</div>
-                            {/* <span className="vr" />
-                            <div>jun 23, 2023</div> */}
+                            <div className="text-primary fw-medium">Azizi Venice</div>
+                           
                         </div>
-                        {/* /.End Meta Info */}
-                        {/* Start Post Title */}
+                        
                         <h5 className="post-title fw-semibold fs-19 mb-3">
                             <Link href="#">
                             Elevate Your Lifestyle at Azizi Venice
                             </Link>
                         </h5>
-                        {/* /.End Post Title */}
+                        
                     </div>
                 </div>
             </div>
@@ -114,7 +112,7 @@ const Carousel = () => {
                     <div className="bg-white m-auto position-relative post-header z-1 pb-0">
                         {/* Start Meta Info */}
                         <div className="align-items-start flex-wrap fs-15 gap-2 hstack text-capitalize mb-1 justify-content-center">
-                            <div className="text-primary fw-medium">A Community with Heart</div>
+                            <div className="text-primary fw-medium">Azizi Revera</div>
                             {/* <span className="vr" />
                             <div>jun 23, 2023</div> */}
                         </div>
@@ -129,54 +127,7 @@ const Carousel = () => {
                     </div>
                 </div>
             </div>
-            {/* /.End Grid Post */}
-            {/* Start Grid Post */}
             
-            {/* /.End Grid Post */}
-            {/* Start Grid Post */}
-            <div className="post grid-post" data-aos="fade" data-aos-delay={600}>
-                <div className="grid-post__thumb position-relative">
-                    <Link
-                        href="Azizi-Venice"
-                        className="align-items-center d-flex justify-content-center overflow-hidden position-relative rounded-3 thumb-overlay"
-                    >
-                        <figure className="w-100 m-0">
-                            <img
-                                src="assets/img/blog/4.jpg"
-                                className="img-fluid w-100"
-                                alt=""
-                            />
-                        </figure>
-                        <div className="bottom-0 opacity-0 position-absolute start-0 thumb-content top-0 w-100 z-1">
-                            <div className="position-absolute text-center top-50 w-100">
-                                <h4 className="d-inline-block fs-12 fw-medium l-spacing-2 link text-white">
-                                    Continue Reading
-                                    <i className="fa fa-angle-right text-white" />
-                                </h4>
-                            </div>
-                        </div>
-                    </Link>
-                </div>
-                <div className="post-info text-center">
-                    <div className="bg-white m-auto position-relative post-header z-1 pb-0">
-                        {/* Start Meta Info */}
-                        <div className="align-items-start flex-wrap fs-15 gap-2 hstack text-capitalize mb-1 justify-content-center">
-                            <div className="text-primary fw-medium">Investment Opportunities</div>
-                            {/* <span className="vr" />
-                            <div>jun 23, 2023</div> */}
-                        </div>
-                        {/* /.End Meta Info */}
-                        {/* Start Post Title */}
-                        <h5 className="post-title fw-semibold fs-17 mb-3">
-                            <Link href="#">
-                            Invest in Azizi Venice: A Prime Opportunity
-                            </Link>
-                        </h5>
-                        {/* /.End Post Title */}
-                    </div>
-                </div>
-            </div>
-            {/* /.End Grid Post */}
         </OwlCarousel>
     );
 };
