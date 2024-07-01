965f1396e72bbf3999da33e4e5d988612fedf185
Fazalwahab12 <wahabdir567@gmail.com>
2024-07-01 06:13:40 -0700

    arow

--- src/app/crescoComponent/FSection.jsx
+++ src/app/crescoComponent/FSection.jsx
@@ -1,10 +1,7 @@
-'use client'
-// import React from "react";
-// import localFont from "next/font/local";
+"use client";
+import { DivideCircleIcon } from "lucide-react";
 import Image from "next/image";
-// // Load fonts with font-display: swap
-// const stroma = localFont({ src: "../fonts/Stroma-Regular.ttf", display: 'swap' });
-
+import { FaArrowDown } from "react-icons/fa6";
 const FSection = () => {
   return (
     <>
@@ -12,9 +9,7 @@ const FSection = () => {
         className="align-items-center d-flex hero-header overflow-hidden position-relative"
         style={{ height: "100vh", width: "100vw" }}
       >
-        
         <div>
-        
           <Image
             src="/assets/img/bg-cresco.jpg"
             layout="fill"
@@ -22,7 +17,7 @@ const FSection = () => {
             className="h-100 w-100 object-fit-cover position-absolute  oblique-image top-0 video"
           />
         </div>
-        
+
         <div
           className="container text-center flex items-center justify-center position-relative"
           style={{ height: "100%", width: "100%" }}
@@ -33,11 +28,13 @@ const FSection = () => {
                 Cresco Real Estate LLC
               </div>
               <h1 className=" text-white  font-bold  text-5xl ">
-                <span >
+                <span>
                   Unlock the Door <br /> To Your Dream Home
                 </span>
               </h1>
-              <h2 className="text-white text-2xl"></h2>
+              <div className="text-white pt-16 px-10 justify-center flex ">
+                <FaArrowDown size={100} />
+              </div>
             </div>
           </div>
         </div>
